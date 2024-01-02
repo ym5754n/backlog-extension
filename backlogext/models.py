1e08b1de0aea8e020c2492fd4289f86fff92d651
ym5754n <29919765+ym5754n@users.noreply.github.com>
2024-01-03 01:08:19 +0900

    課題テーブルの追加

--- backlogext/models.py
+++ backlogext/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Issue(models.Model):
+    summary = models.CharField('件名', max_length=255)
+    description = models.TextField('詳細')
+
+    def __str__(self):
+        return self.summary
