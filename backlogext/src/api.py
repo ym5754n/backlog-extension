736d6837bc0f08ced0fd64a23dd898558561c4b8
ym5754n <29919765+ym5754n@users.noreply.github.com>
2024-01-14 11:26:38 +0900

    APIリクエストURLのスペースキーをsettingから取得するよう変更

--- backlogext/src/api.py
+++ backlogext/src/api.py
@@ -1,51 +1,52 @@
 import requests
 from ..models import Setting, Token
-from . import util
 
+class BacklogApi:
+    def __init__(self, user):
+        self.setting = Setting.objects.get(user=user)
+        self.token = Token.objects.get(user=user)
+        self.base_url = f'https://{self.setting.space_key}.backlog.jp/api/v2/'
 
-def post(url, data, headers=None):
-    """POSTでAPIリクエストを実行する"""
-    r = requests.post(url, headers=headers, json=data)
-    jsonData = r.json()
+    def post(self, url, data, headers=None):
+        """POSTでAPIリクエストを実行する"""
+        r = requests.post(url, headers=headers, json=data)
+        jsonData = r.json()
 
-    print("response", jsonData)
-    return jsonData
+        print("response", jsonData)
+        return jsonData
 
-def create_issue(header, data):
-    """課題の追加 /api/v2/issues"""
-    url = 'https://nulab-exam.backlog.jp/api/v2/issues'
+    def create_issue(self, header, data):
+        """課題の追加 /api/v2/issues"""
+        url = self.base_url + 'issues'
 
-    print("request", data)
-    return post(url, data, header)
+        print("request", data)
+        return self.post(url, data, header)
 
-def refresh_token(user):
-    """アクセストークンの更新 /api/v2/oauth2/token"""
-    url = 'https://nulab-exam.backlog.jp/api/v2/oauth2/token'
-    setting = Setting.objects.get(user=user)
-    token = Token.objects.get(user=user)
+    def refresh_token(self):
+        """アクセストークンの更新 /api/v2/oauth2/token"""
+        url = self.base_url + 'oauth2/token'
 
-    data = {
-        'grant_type': 'refresh_token',
-        'client_id': setting.client_id,
-        'client_secret': setting.client_secret,
-        'refresh_token': token.refresh_token,
-    }
+        data = {
+            'grant_type': 'refresh_token',
+            'client_id': self.setting.client_id,
+            'client_secret': self.setting.client_secret,
+            'refresh_token': self.token.refresh_token,
+        }
 
-    print("request", data)
-    return post(url, data)
+        print("request", data)
+        return self.post(url, data)
 
-def create_token(user):
-    """アクセストークンリクエスト /api/v2/oauth2/token"""
-    url = 'https://nulab-exam.backlog.jp/api/v2/oauth2/token'
-    setting = Setting.objects.get(user=user)
+    def create_token(self):
+        """アクセストークンリクエスト /api/v2/oauth2/token"""
+        url = self.base_url + 'oauth2/token'
 
-    data = {
-        'grant_type': 'authorization_code',
-        'code': setting.code,
-        'redirect_uri': 'http://localhost:8000/authenticate_success',
-        'client_id': setting.client_id,
-        'client_secret': setting.client_secret,
-    }
+        data = {
+            'grant_type': 'authorization_code',
+            'code': self.setting.code,
+            'redirect_uri': 'http://localhost:8000/authenticate_success',
+            'client_id': self.setting.client_id,
+            'client_secret': self.setting.client_secret,
+        }
 
-    print("request", data)
-    return post(url, data)
+        print("request", data)
+        return self.post(url, data)
--- backlogext/views/api.py
+++ backlogext/views/api.py
@@ -6,8 +6,11 @@ from django.contrib import messages
 
 def authenticate_success(request):
     """oauth2の認可コード取得完了時の処理を行う"""
+
+    backlog_api = api.BacklogApi(request.user)
+
     db.update_setting_code(request.GET['code'], request.user)
-    jsonData = api.create_token(request.user)
+    jsonData = backlog_api.create_token()
     db.update_token(jsonData, request.user)
 
     messages.info(request, f'backlogとの連携が完了しました。再度｢追加｣を実行してください')
@@ -15,6 +18,9 @@ def authenticate_success(request):
 
 def create_issue(request):
     """課題をbacklogに追加する"""
+
+    backlog_api = api.BacklogApi(request.user)
+
     try:
         setting = Setting.objects.get(user=request.user.id)
     except Setting.DoesNotExist:
@@ -33,11 +39,11 @@ def create_issue(request):
         'issueTypeId': request.POST['issueTypeId'],
         'priorityId': request.POST['priorityId'],
     }
-    json_response = api.create_issue(header, body)
+    json_response = backlog_api.create_issue(header, body)
 
     # tokenが有効期限切れの場合、tokenを再発行する
     if 'errors' in json_response and json_response['errors'][0]['code'] == 11:
-        token = api.refresh_token(request.user)
+        token = backlog_api.refresh_token()
         db.update_token(token, request.user)
         messages.info(request, f'再度実行してください')
         return redirect('/issue_list')
